8b9b06d1354b179353f7dab012acbee30feb50c9
lvsheng <252939036@qq.com>
2018-12-13 16:02:54 +0800

    图标dubbo接口

---
+++ src/main/java/com/iot/market/rpc/interfaces/service/AppImageService.java
@@ -0,0 +1,29 @@
+package com.iot.market.rpc.interfaces.service;
+
+
+import com.iot.market.rpc.interfaces.bean.AppImage;
+import com.iot.market.rpc.interfaces.bean.Instant;
+import com.iot.market.rpc.interfaces.util.ResMsg;
+
+import java.util.List;
+
+/**
+ * Created by lvsheng on 2018/12/13.
+ */
+public interface AppImageService {
+
+    /**
+     * 获取一级列表
+     * @return
+     */
+    List<AppImage> queyrAppImageList(AppImage param) throws Exception;
+
+
+    /**
+     * 查询设备详情图片
+     * @return
+     */
+    AppImage queyrAppImage(Instant param) throws Exception;
+
+
+}
