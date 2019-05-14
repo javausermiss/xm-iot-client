f5e5fdf5f18b961bef520e4cf55b58376e66b9a9
张大伟 <p_dawzhang@tencent.com>
2019-05-14 17:16:17 +0800

    新增普通类定时器接口

--- src/main/java/com/iot/market/rpc/interfaces/RpcJobService.java
+++ src/main/java/com/iot/market/rpc/interfaces/RpcJobService.java
@@ -7,14 +7,34 @@ import com.iot.market.rpc.interfaces.util.ResMsg;
  */
 public interface RpcJobService {
 
+    /**
+     * 获取定时器
+     */
     String getJob(String group, String name);
 
+    /**
+     * 删除定时器
+     */
     ResMsg<String> removeJob(String group, String name);
 
+    /**
+     * 根据分组获取定时器
+     */
     String getJobByGroup(String group);
 
+    /**
+     * 添加批处理定时器
+     */
     ResMsg<String> addBatchPowerJob(String jsonJob);
 
+    /**
+     * 添加普通类定时器
+     */
+    ResMsg<String> addNormalJob(String jsonJob);
+
+    /**
+     * 添加倒计时定时器
+     */
     ResMsg<String> addTimerJob(String jsonJob);
 
     ResMsg<String> switchJob(String group, String name, Boolean enable);
@@ -23,6 +43,9 @@ public interface RpcJobService {
 
     ResMsg<String> deleteTrigger(String triggerGroup, String triggerName);
 
+    /**
+     * 定时器开关
+     */
     ResMsg<String> switchTrigger(String triggerGroup, String triggerName, Boolean enable);
 
     ResMsg<String> getTriggerByKey(String jobGroup, String jobName);
